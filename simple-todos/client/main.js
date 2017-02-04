50206bea1fbe9d174a4038b7e2d95be851e82566
jkennan <jkennan97@gmail.com>
2017-02-04 18:12:10 -0500

    Upload the todos app for manipulation

---
+++ simple-todos/client/main.js
@@ -0,0 +1,4 @@
+import { Meteor } from 'meteor/meteor';
+import '../imports/startup/accounts-config.js';
+import '../imports/ui/body.js';
+
---
+++ simple-todos/server/main.js
@@ -0,0 +1,5 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(() => {
+ import '../imports/api/tasks.js';
+});
