3fe05ace768ec26ed30b34badd019ba3e2b3f20e
Jacob Leiken <jacobleiken@gmail.com>
2017-02-05 11:21:42 -0500

    fsdklkhfs

--- imports/ui_new/host_index.js
+++ imports/ui_new/host_index.js
@@ -1,11 +1,12 @@
 import {Meteor} from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Tasks } from '../api/tasks.js';
-import { YouTubeAPI } from '../api/youtubeapi.js';
+import oxford from 'project-oxford';
 
 import './task.js';
-
+//import './mood_manager.js';
 import './host_index.html';
+
 
 Template.host_index.helpers({
 	tasks() {
@@ -29,5 +30,32 @@ Template.host_index.events({
 
 		target.text.value = '';
 		target.url.value = '';
+	},
+});
+
+Meteor.methods({
+	analyzeIm: function (img) {
+	const oxford = require('project-oxford');
+	var key = "cf8a98ef55764e0bb3129e66dd8b3294";	
+	var client = new oxford.Client(key);
+
+	client.emotion.analyzeEmotion({
+		data: img,
+	}).then(function (emotionresponse) {
+		console.log(emotionresponse);
+
+		var happyscore = 0;
+
+		emotionresponse.forEach(function (element) {
+			var happiness = element.scores.happiness
+			var sadness = element.scores.sadness
+
+			if (happiness > sadness)
+				happyscore = happyscore + 1
+			else
+				happyscore = happyscore - 1
+		}, this);
+		return happyscore;
+		});
 	},
 });
---
+++ imports/ui_new/mood_manager.js
@@ -0,0 +1,91 @@
+var express = require('express');
+var router = express.Router();
+
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+var oxford = require('project-oxford');
+var fs = require('fs')
+	, gm = require('gm');
+var magick = gm.subClass({imageMagick: true});
+var key = "cf8a98ef55764e0bb3129e66dd8b3294";
+
+/* GET home page. */
+/*router.get('/', function (req, res, next) {
+	res.render('host_index', { title: 'Express' });
+});
+
+router.get('/result', function (req, res, next) {
+	res.render('result', { title: 'Express' });
+});
+
+router.post('/upload', upload.single('thumbnail'), function (req, res, next) {
+	console.log('path of request file:', req.file.path);*/
+
+function analyzeIm(img) {
+	var client = new oxford.Client(key);
+
+	client.emotion.analyzeEmotion({
+		path: img.src,
+	}).then(function (emotionresponse) {
+		console.log(emotionresponse);
+
+		var happyscore = 0;
+
+		var draw = magick(req.file.path);
+		emotionresponse.forEach(function (element) {
+			var x = element.faceRectangle.left;
+			var y = element.faceRectangle.top;
+			var x1 = element.faceRectangle.left + element.faceRectangle.width;
+			var y1 = element.faceRectangle.top + element.faceRectangle.height;
+
+			console.log("transparent")
+			draw.stroke("red", 2)
+				.fill("rgba(124,240,10,0.0)")  //transparent box
+				.drawRectangle(x, y, x1, y1)
+
+			var happiness = element.scores.happiness
+			var sadness = element.scores.sadness
+
+			if (happiness > sadness)
+				happyscore = happyscore + 1
+			else
+				happyscore = happyscore - 1
+		}, this);
+
+		var tempfile = 'uploads/file' + random(0, 3000) + '.jpg';
+		draw.write(tempfile, function (err) {
+			if (err) console.log(err);
+			var modifiedimage = base64_encode(tempfile);
+
+			// would be better to never store file.  
+			// would need to update projectoxford to work with in memory files.
+			fs.unlink(req.file.path, function (err) {
+				if (err) throw err;
+				console.log('successfully deleted tmp file');
+			});
+
+			fs.unlink(tempfile, function (err) {
+				if (err) throw err;
+				console.log('successfully deleted /tmp/hello');
+			});
+
+			res.render('result', { modified: modifiedimage, isHappy: happyscore });
+		});
+
+	});
+}  
+
+//http://www.hacksparrow.com/base64-encoding-decoding-in-node-js.html
+// function to encode file data to base64 encoded string
+function base64_encode(file) {
+	// read binary data
+	var bitmap = fs.readFileSync(file);
+	// convert binary data to base64 encoded string
+	return new Buffer(bitmap).toString('base64');
+}
+
+function random(low, high) {
+	return Math.random() * (high - low) + low;
+}
+
+module.exports = router;
