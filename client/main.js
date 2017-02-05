2a2d420b0cbd58a4ec0425c90a9e5ba61754e0f2
MckennaCisler <mckennacisler@gmail.com>
2017-02-04 21:38:00 -0500

    Finish getting paths to work

--- client/main.js
+++ client/main.js
@@ -2,18 +2,40 @@ import { Meteor } from 'meteor/meteor';
 import { IronRouter } from 'meteor/iron:router';
 
 import './bootstrap.css'
-import '../imports/startup/accounts-config.js';
-import '../imports/ui_new/home.js';
+//import '../imports/startup/accounts-config.js';
+import '../imports/ui_new/index.html';
 import '../imports/ui_new/make-party.html';
+import '../imports/ui_new/add-song.html';
+import '../imports/ui_new/guest-index.html';
+import '../imports/ui_new/host-index.html';
+import '../imports/ui_new/make-guest.html';
 
 Router.route('/', function () {
-  this.render('home');
+  this.render('index');
 });
 
-Router.route('/home', function () {
-  this.render('home');
+Router.route('/index', function () {
+  this.render('index');
 });
 
 Router.route('/make-party', function () {
   this.render('make-party');
 });
+
+Router.route('/add-song', function () {
+  this.render('add-song');
+});
+
+Router.route('/guest-index', function () {
+  this.render('guest-index');
+});
+
+Router.route('/host-index', function () {
+  this.render('host-index');
+});
+
+Router.route('/make-guest', function () {
+  this.render('make-guest');
+});
+
+
--- imports/ui_new/home.js
+++
@@ -1,5 +0,0 @@
-import { Template } from 'meteor/templating';
-import { IronRouter } from 'meteor/iron:router';
-
-import './home.html';
-
