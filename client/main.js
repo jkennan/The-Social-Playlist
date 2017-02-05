38c10dd8259901eeb2c1f914f812d11b4f368f60
MckennaCisler <mckennacisler@gmail.com>
2017-02-04 21:55:24 -0500

    Add contact and about-us

--- client/main.js
+++ client/main.js
@@ -2,13 +2,16 @@ import { Meteor } from 'meteor/meteor';
 import { IronRouter } from 'meteor/iron:router';
 
 import './bootstrap.css'
-//import '../imports/startup/accounts-config.js';
+import '../imports/startup/accounts-config.js';
 import '../imports/ui_new/index.html';
 import '../imports/ui_new/make-party.html';
 import '../imports/ui_new/add-song.html';
 import '../imports/ui_new/guest-index.html';
 import '../imports/ui_new/host-index.html';
 import '../imports/ui_new/make-guest.html';
+import '../imports/ui_new/about-us.html';
+import '../imports/ui_new/contact.html';
+
 
 Router.route('/', function () {
   this.render('index');
@@ -36,6 +39,14 @@ Router.route('/host-index', function () {
 
 Router.route('/make-guest', function () {
   this.render('make-guest');
+});
+
+Router.route('/about-us', function () {
+  this.render('about-us');
+});
+
+Router.route('/contact', function () {
+  this.render('contact');
 });
 
 
